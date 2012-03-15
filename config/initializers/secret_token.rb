7035390619b5b55ea0ec9de37855f79add0fafc0
John Quarto-vonTivadar <github@johnquarto.com>
2012-03-15 13:06:35 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quartolove::Application.config.secret_token = '8d329bdaef02f576460e1e08e3ada7c5f40f8cf989e80c7b8a51491e411ecb949b271baff92b19f0f583384d72e7d276a7a3a53dd66851ba95753bc60238abaa'
